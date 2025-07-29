1ba5c7fe2ec54c4399bc95ab7fc39b66b8509369
samdev009 <s.godoy.developer@gmail.com>
2025-07-28 21:44:37 -0600

    Revert "Actualización de next.config.js"
    This reverts commit ff38061b5291be6f85b688c3b4c1f5757d07e470.

--- next.config.js
+++ next.config.js
@@ -1,59 +1,56 @@
-// next.config.js
 // @ts-check
 
-const crypto = require('crypto');
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' https://apis.google.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
+  font-src 'self';
+  connect-src 'self';
+  frame-ancestors 'none';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+  key: 'Strict-Transport-Security',
+  value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false, 
 
-  async headers() { 
+  async headers() {
     return [
       {
-        source: '/(.*)',
-
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: (() => {
-              const nonce = crypto.randomBytes(16).toString('base64');
-              const ContentSecurityPolicy = `
-                default-src 'self';
-                script-src 'self' 'nonce-${nonce}' https://apis.google.com;
-                style-src 'self' 'nonce-${nonce}';
-                img-src 'self' data:;
-                font-src 'self';
-                connect-src 'self';
-                frame-ancestors 'none';
-              `;
-              return ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim();
-            })(), 
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=63072000; includeSubDomains; preload',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
+        source: '/(.*)', // Aplica a todas las rutas
+        headers: securityHeaders,
       },
     ];
   },
 };
 
 module.exports = nextConfig;
+
